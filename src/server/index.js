d129e4b488df218b79aad6e921fd3463db7b107b
karlwes <karl.westgardh@gmail.com>
2020-01-01 07:54:55 +0100

    fix error handling on atlas connection

--- src/server/index.js
+++ src/server/index.js
@@ -9,7 +9,6 @@ const uri = application_keys.getKeys.connection_string
 var productionsController = require('./routes/productions')
 var usersController = require('./routes/users')
 
-
 //Setup of Okta JWTVerifier
 oktaJwtVerifier = new oktaJwtVerifier({
     issuer: `${application_keys.getKeys.okta_domain}/oauth2/default`,
@@ -30,15 +29,47 @@ app.use(bodyParser.json())
 @ useUnifiedTopology : required for mongoose to be able to monitor server
 @ useFindAndModify : required to be able to use methods update methods such as findByIdAndUpdate
 */
-mongoose.connect(uri,{useNewUrlParser:true, useUnifiedTopology:true, useFindAndModify: false});
-//https://stackoverflow.com/questions/16226472/mongoose-autoreconnect-option
+function connect(){
+  setTimeout(function(){
+    mongoose.connect(uri,
+      {
+        useNewUrlParser:true, 
+        useUnifiedTopology:true, 
+        useFindAndModify: false,
+        connectTimeoutMS: 10000, //give up initial connection after 10 seconds
+      });
+  },10000)
+}
+//call connect
+connect();
 
 //Connection handling
 var db = mongoose.connection;
-db.on('error',console.error.bind(console,'- Status: Failed to connect to MongoDB Atlas'))
-db.once('open', function(){
-    console.log('- Status: Succesfully connected to MongoDB Atlas')
+db.on('error', function(){
+  //triggered when an error 
+  console.error.bind(console,`- Status: Failed to connected to MongoDB! @ ${new Date().toString()}`)
+
+  //reconnect
+  connect()
 })
+db.once('open', function(){
+  //triggerd when connection is open (final state)
+    console.log(`- Status: Successfully connected to MongoDB! @ ${new Date().toString()}`)
+})
+db.on('connecting', function() {
+  //triggered when connected to wi-fi but there is no internet access
+  console.log(`- Status: Connecting to MongoDB! @ ${new Date().toString()}`);
+});
+db.on('connected', function() {
+  console.log(`- Status: MongoDB connected! @ ${new Date().toString()}`);
+});
+db.on('reconnected', function () {
+  console.log(`- Status: MongoDB reconnected! @ ${new Date().toString()}`);
+});
+db.on('disconnected', function() {
+  //triggered before error
+  console.log(`- Status: MongoDB disconnected! @ ${new Date().toString()}`);
+});
 
 // verify JWT token middleware
 app.use((req, res, next) => {
